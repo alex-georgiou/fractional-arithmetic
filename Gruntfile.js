61211f1f3e6da2845f18060198151fb4de7b9801
alex-georgiou <alex.georgiou@gmail.com>
2016-01-20 13:29:56 +0200

    adding grunt task to automate minification

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+	grunt
+		.initConfig({
+			pkg : grunt.file.readJSON('package.json'),
+			
+			clean: ['*.min.js'],
+
+			uglify : {
+				txtfx: {
+					expand : true, // required when using cwd
+					src: ['fractional-arithmetic.js'],
+					ext: '.min.js',
+					extDot: 'first',
+					options : {
+						banner : '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+					},
+				}
+			},
+			
+			jshint: {
+				src: ['Gruntfile.js', 'fractional-arithmetic.js'],
+			},
+			
+		});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	
+	grunt.registerTask('default',['jshint']);
+	grunt.registerTask('build', ['clean', 'uglify']);
+};
