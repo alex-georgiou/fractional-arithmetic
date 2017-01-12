4cd8b53658d45f02d5bb6315b35157a97f227d98
Alexandros Georgiou <alex.georgiou@gmail.com>
2017-01-12 21:07:17 +0200

    readability

--- test/transform_test.js
+++ test/transform_test.js
@@ -2,6 +2,7 @@ var Fraction = require( '../fractional-arithmetic.js' ).Fraction;
 
 exports.fraction_to_formats_tests = function( test ) {
 	test.expect( 6 );
+
 	var f = Fraction( 7, 3 );
 	test.equal( f.toString(), '(7/3)', 'toString' );
 	test.equal( f.toS(), '(7/3)', 'toS' );
@@ -9,19 +10,24 @@ exports.fraction_to_formats_tests = function( test ) {
 	test.equal( f.toNumber(), 7/3, 'toNumber' );
 	test.equal( f.toLatex(), '\\frac{7}{3}', 'toLatex' );
 	test.equal( f.toMathML(), '<mfrac><mn>7</mn><mn>3</mfrac>', 'toMathML' );
+
 	test.done();
 };
 
 exports.fraction_simpify_tests = function( test ) {
 	test.expect( 6 );
+
 	var f = Fraction( 7, 3 ).simplify();
 	test.equal( f.n, 7, 'numerator when not simplifiable' );
 	test.equal( f.d, 3, 'denominator when not simplifiable' );
+
 	var g = Fraction( 100, 25 ).simplify();
 	test.equal( g.n, 4, 'numerator when simplifiable' );
 	test.equal( g.d, 1, 'denominator when simplifiable' );
+
 	var h = Fraction( 4, 8 ).simplify();
 	test.equal( h.n, 1, 'numerator when simplifiable' );
 	test.equal( h.d, 2, 'denominator when simplifiable' );
+
 	test.done();
 };
