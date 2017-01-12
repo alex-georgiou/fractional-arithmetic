7a14e6f3da799eb89eacb7e51e5b8ba00660c95d
Alexandros Georgiou <alex.georgiou@gmail.com>
2017-01-12 19:33:51 +0200

    using gitinfo in minified header comment

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,9 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 			pkg : grunt.file.readJSON('package.json'),
-			
+
+			gitinfo: { },
+
 			clean: ['*.min.js'],
 
 			uglify : {
@@ -12,21 +14,24 @@ module.exports = function(grunt) {
 					ext: '.min.js',
 					extDot: 'first',
 					options : {
-						banner : '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+						banner :	'/*<%= pkg.name %> <%= pkg.version %> ' +
+									'<%= gitinfo.local.branch.current.shortSHA %> ' +
+									"<%= gitinfo.local.branch.current.lastCommitTime %> */\n"
 					},
 				},
 			},
-			
+
 			jshint: {
 				src: [ 'Gruntfile.js', 'fractional-arithmetic.js' ],
 			},
-			
+
 		});
 
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-contrib-clean' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-gitinfo' );
 	
-	grunt.registerTask( 'default', ['jshint'] );
-	grunt.registerTask( 'build', ['clean', 'uglify']);
+	grunt.registerTask( 'default', [ 'jshint' ] );
+	grunt.registerTask( 'build', [ 'clean', 'gitinfo', 'uglify' ]);
 };
