2a0d12d0f8f96b4f32be3e8ffad1100842318a2e
Alexandros Georgiou <alex.georgiou@gmail.com>
2017-01-12 20:28:19 +0200

    testing isInteger

--- test/helpers_test.js
+++ test/helpers_test.js
@@ -1,6 +1,18 @@
-exports.gcd_tests = function(test) {
+exports.isInteger_tests = function( test ) {
+	var isInteger = require( '../fractional-arithmetic.js' ).isInteger;
+
+	test.expect( 5 );
+	test.ok( isInteger( 1 ), 'one' );
+	test.ok( isInteger( 654321 ), '654321' );
+	test.ok( isInteger( '1' ), 'string 1' );
+	test.ok( !isInteger( NaN ), 'NaN' );
+	test.ok( !isInteger( 1.5 ), '1.5' );
+	test.done();
+};
+
+exports.gcd_tests = function( test ) {
 	var gcd = require( '../fractional-arithmetic.js' ).gcd;
-	
+
     test.expect( 4 );
     test.equal( gcd( 0, 0 ), 0, 'gcd of 0 and 0' );
     test.equal( gcd( 0, 1 ), 1, 'gcd of 0 and 1' );
@@ -9,9 +21,9 @@ exports.gcd_tests = function(test) {
     test.done();
 };
 
-exports.lcm_tests = function(test) {
+exports.lcm_tests = function( test ) {
 	var lcm = require( '../fractional-arithmetic.js' ).lcm;
-	
+
     test.expect( 4 );
     test.ok( isNaN( lcm( 0, 0 ) ), 'lcm of 0 and 0' );
     test.equal( lcm( 0, 1 ), 0, 'lcm of 0 and 1' );
