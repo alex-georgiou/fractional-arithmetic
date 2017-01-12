5ee64017e45f479ab0ab161755cad873dbf029f4
Alexandros Georgiou <alex.georgiou@gmail.com>
2017-01-12 21:04:01 +0200

    refactoring

---
+++ test/arithmetic_test.js
@@ -0,0 +1,26 @@
+var Fraction = require( '../fractional-arithmetic.js' ).Fraction;
+
+exports.fraction_inverse_tests = function( test ) {
+	test.expect( 0 );
+	test.done();
+};
+
+exports.fraction_times_tests = function( test ) {
+	test.expect( 0 );
+	test.done();
+};
+
+exports.fraction_dividedBy_tests = function( test ) {
+	test.expect( 0 );
+	test.done();
+};
+
+exports.fraction_plus_tests = function( test ) {
+	test.expect( 0 );
+	test.done();
+};
+
+exports.fraction_minus_tests = function( test ) {
+	test.expect( 0 );
+	test.done();
+};
